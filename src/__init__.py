658bf248993cfac5fd166b4e44b12a3b8080cb5c
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-29 16:12:18 +0545

    importing necessary function

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
-from .preprocess import clean_sentences
-from .count import frequency_of_words, each_word_frequency_in_sentence
+from .preprocess import CleanText
+from .summary import tf_idf_score, average_score, text_summary
+from .tfidf import tf_evaluation, idf_evaluation, tf_idf_evaluation
+from .count import frequency_of_words, each_word_frequency_in_sentence, sentence_frequency_of_each_word
