3101a4719729b6fa0914a66e666199dc95d7807d
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-29 23:15:38 +0545

    importing necessary function

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from .preprocess import CleanText
+from .speech_to_text import speech_text
 from .summary import tf_idf_score, average_score, text_summary
 from .tfidf import tf_evaluation, idf_evaluation, tf_idf_evaluation
 from .count import frequency_of_words, each_word_frequency_in_sentence, sentence_frequency_of_each_word
