e04e21481c84ce30889965b41619e1fccff7b6b3
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-30 19:49:17 +0545

    added test of functions

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,5 +1,5 @@
 import sys
-
+# sys.path.insert(0, r'F:\Leapfrog Technology\Major Project')
 import pytest
 
 from src import CleanText
@@ -16,6 +16,7 @@ def test_clean_sentences():
     assert type(output_1.clean_sentences()) == str
     assert output_1.clean_sentences() == "Programming is fun."
     assert output_2.clean_sentences() == " Programming is fun."
+    assert output_2.clean_sentences() != "Programming is fun"
 
 
 # Testing of frequency_of_words function
@@ -23,6 +24,8 @@ def test_count_words():
     output = frequency_of_words('Programming is fun.')
     assert type(output) == dict
     assert output == {'fun': 1, 'program': 1}
+    assert output != {'fun': 1, 'Program': 1}
+    assert output != {'fun': 1, 'programming': 1}
 
 
 # Testing of each_word_frequency_in_sentence function
@@ -39,6 +42,7 @@ def test_sentence_frequency_of_each_word():
     output = sentence_frequency_of_each_word(text)
     assert type(output) == dict
     assert output == {'love': 1, 'code': 1}
+    assert output != {'we': 1, 'love': 1, 'to': 1, 'do': 1, 'code': 1}
 
 
 # Testing of tf_evaluation function
@@ -50,6 +54,7 @@ def test_tf():
     assert type(output_1) == dict
     assert output_1 == {'We love to do coding in python.': {'code': 0.33, 'love': 0.33, 'python': 0.33}}
     assert output_2 == {'We love to do coding in python.': {'code': 0.33, 'love': 0.67, 'python': 0.33}}
+    assert output_2 != {'We love to do coding in python.': {'code': 0.25, 'love': 0.50, 'python': 0.25}}
 
 
 # Testing of idf_evaluation function
@@ -60,6 +65,7 @@ def test_idf():
     assert type(output) == dict
     assert type(output) != str
     assert output == {'We love to do coding in python.': {'code': 0.69, 'love': 0.41, 'python': 0.69}}
+    assert output != {'We love to do coding in python.': {'code': 0, 'love': 0.39, 'python': 0}}
 
 
 # Testing of tf_idf_evaluation function
@@ -84,13 +90,15 @@ def test_average_score():
     dict_example = {'We love to do coding in python.': 0.6}
     output = average_score(dict_example)
     assert type(output) == float
+    assert type(output) != str
     assert output == 0.6
 
 
 # Testing of text_summary function
-def test_summary_5():
+def test_summary():
     text = 'We love to do coding in python.'
     dict_example = {'We love to do coding in python.': 0.6}
     output = text_summary(text, dict_example, 0.6)
     assert type(output) == str
     assert output == ' We love to do coding in python.'
+    assert output != ' We love to do coding in python'
