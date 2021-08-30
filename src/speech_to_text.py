6e4605f4c69a68c9325b48e73bf1c4c0209d37fd
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-30 19:48:49 +0545

    update to AudioText class

--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -9,35 +9,39 @@ from pydub.silence import split_on_silence
 r = sr.Recognizer()
 
 
-def speech_text(path: str) -> str:
-    """
-        Return text converted from the input audio using google recognizer.
+class AudioText:
+    def __init__(self, path: str):
+        self.path = path
 
-        Inputs: path of the audio file
+    def speech_text(self) -> str:
+        """
+            Return text converted from the input audio using google recognizer.
 
-        Returns:
-        - text in string.
-    """
+            Inputs: path of the audio file
 
-    sound = AudioSegment.from_wav(path)
-    chunks = split_on_silence(sound, min_silence_len=500, silence_thresh=sound.dBFS-14, keep_silence=500,)
-    head, tail = ntpath.split(path)
-    to_be_folder = os.path.splitext(tail)[0]
-    folder_name = "Media/" + to_be_folder + "-chunks"
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
-    text_from_audio = ""
-    for i, audio_chunk in enumerate(chunks, start=1):
-        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
-        audio_chunk.export(chunk_filename, format="wav")
-        with sr.AudioFile(chunk_filename) as source:
-            audio_note = r.record(source)
-            try:
-                text = r.recognize_google(audio_note)
-            except sr.UnknownValueError as e:
-                print("Error:", str(e))
-            else:
-                text = f"{text.capitalize()}. "
-                text_from_audio = text_from_audio + text
+            Returns:
+            - text in string.
+        """
 
-    return text_from_audio
+        sound = AudioSegment.from_wav(self.path)
+        chunks = split_on_silence(sound, min_silence_len=500, silence_thresh=sound.dBFS-14, keep_silence=500,)
+        head, tail = ntpath.split(self.path)
+        to_be_folder = os.path.splitext(tail)[0]
+        folder_name = "Media/" + to_be_folder + "-chunks"
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
+        text_from_audio = ""
+        for i, audio_chunk in enumerate(chunks, start=1):
+            chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+            audio_chunk.export(chunk_filename, format="wav")
+            with sr.AudioFile(chunk_filename) as source:
+                audio_note = r.record(source)
+                try:
+                    text = r.recognize_google(audio_note)
+                except sr.UnknownValueError as e:
+                    print("Error:", str(e))
+                else:
+                    text = f"{text.capitalize()}. "
+                    text_from_audio = text_from_audio + text
+
+        return text_from_audio
