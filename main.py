767d05b6dea533a59d3501280880678117eb26ee
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-30 19:47:42 +0545

    display using streamlit

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import os
+
+import streamlit as st
+
 from src import CleanText
+from src import AudioText
 from src import tf_idf_score, average_score, text_summary
 from src import tf_evaluation, idf_evaluation, tf_idf_evaluation
 from src import each_word_frequency_in_sentence, sentence_frequency_of_each_word
+
+
+def audio_to_text(path: str) -> str:
+    """
+        Return text converted from the input audio using google recognizer.
+
+        Inputs: path of the audio file
+
+        Returns:
+        - text in string.
+    """
+
+    audio_note = AudioText(path)
+    audio_text = audio_note.speech_text()
+
+    return audio_text
 
 
 def text_summarization(text: str) -> str:
@@ -34,5 +55,28 @@ def text_summarization(text: str) -> str:
     return summary
 
 
-# if __name__ == "__main__":
-#     main()
+def main():
+    st.title("Audio to Text Summarization")
+    st.subheader("Upload audio file in wav format")
+    audio_file = st.file_uploader("File", type=['wav'])
+    path = ''
+
+    if audio_file is not None:
+        st.audio(audio_file)
+        with open(os.path.join("Media", audio_file.name), "wb") as f:
+            f.write(audio_file.getbuffer())
+        path = 'Media' + '/' + audio_file.name
+
+        text_of_audio = audio_to_text(path)
+
+        text = text_of_audio
+        st.subheader("Text from audio")
+        st.success('{}'.format(text))
+
+        result = text_summarization(text)
+        st.subheader("Text Summary")
+        st.success('{}'.format(result))
+
+
+if __name__ == '__main__':
+    main()
